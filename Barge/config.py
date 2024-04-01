824d7be2b0a8a8514ebb81c742f08845f525c1d3
moeendehqan <moeen.dehqan@gmail.com>
2024-04-01 23:55:42 +0330

    ..

--- Barge/config.py
+++ Barge/config.py
@@ -1,10 +1,20 @@
+from mongoengine import connect
 
 class Config:
     DEBUG = False
     TESTING = False
+    DBNAME = 'ibarge'
+    MONGODB_PORT = 27017
     
+    MONGODB_SETTINGS = {
+        'host': f'mongodb://localhost:27017/{DBNAME}',
+        'authentication_source': 'admin',
+        'connect': False  # برای اتصال به موقعیتی که مطمئنیم از آن استفاده می‌کنیم
+    }
     HOST = '0.0.0.0'
     PORT = 8081
+
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
@@ -18,4 +28,4 @@ config = {
     'default': DevelopmentConfig
 }
 
-
+connect(**Config.MONGODB_SETTINGS)
